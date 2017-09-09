a13e709e4841e740553ad55f401f97f84836003d
Abdurrahman Trimanto <capteinn@gmail.com>
2017-09-09 07:25:59 +0700

    menambahkan file 06_C2_09434.php sekaligus membuat data array 10 mahasiswa

---
+++ 06_C2_09434.php
@@ -0,0 +1,57 @@
+<body>
+	<?php
+		$mahasiswa = array(
+			array(
+				"nim" => "8444",
+				"nama" => "HILMAWAN KHIBATUL",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			),array(
+				"nim" => "6422",
+				"nama" => "ABDURRAHMAN TRIMANTO",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			),array(
+				"nim" => "9653",
+				"nama" => "JOGJA TINNA A",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			),array(
+				"nim" => "2134",
+				"nama" => "SUNGKONO PUJI P",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			),array(
+				"nim" => "9876",
+				"nama" => "RIZKI MAIDA",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			),array(
+				"nim" => "4233",
+				"nama" => "AULIA KUKUH SAPUTRA",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			),array(
+				"nim" => "1297",
+				"nama" => "BIAS ALAMSYAH",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			),array(
+				"nim" => "8765",
+				"nama" => "ERLINA CAHYANI",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			),array(
+				"nim" => "6922",
+				"nama" => "HAMDAN AKBAR",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			),array(
+				"nim" => "3988",
+				"nama" => "ACHIS RIZAL MZR",
+				"kelas" => "C2",
+				"prodi" => "KOMSI"
+			)
+		);
+	?>
+</body>
